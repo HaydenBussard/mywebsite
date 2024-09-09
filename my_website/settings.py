7d74a5a3f0ded32aea320fa928be23c82f7d1afd
Hayden <haydenbussard@outlook.com>
2024-09-09 19:14:48 -0400

    Add views.py and update URL configuration

--- my_website/settings.py
+++ my_website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_website',  # Add your app name here
 ]
 
 MIDDLEWARE = [

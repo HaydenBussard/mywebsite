f53981e450c47d7e38f04d348fa9bae67efea229
Hayden <haydenbussard@outlook.com>
2024-09-14 16:59:18 -0400

    Force port variable to be INT

--- my_website/settings.py
+++ my_website/settings.py
@@ -40,8 +40,8 @@ ALLOWED_HOSTS = [
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS').islower()
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
